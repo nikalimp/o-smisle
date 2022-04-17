a67b9af3c6763005b83695ec5180c051f79bb9d1
Nikita <nikitaaalimpiev@gmail.com>
2022-04-17 19:00:19 +0500

    Initial Commit

---
+++ o'smisle/o_smisleApp.swift
@@ -0,0 +1,17 @@
+//
+//  o_smisleApp.swift
+//  o'smisle
+//
+//  Created by Никита Алимпиев on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct o_smisleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
