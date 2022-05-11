e5423343b0bbac36f0702640e31014c39ac492e7
Nikita <nikitaaalimpiev@gmail.com>
2022-05-11 22:27:08 +0500

    Add navigation after login

---
+++ o'smisle/EmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyView.swift
+//  o'smisle
+//
+//  Created by Никита Алимпиев on 11.05.2022.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyView()
+    }
+}
--- o'smisle/presentation/views/LoginView.swift
+++ o'smisle/presentation/views/LoginView.swift
@@ -9,15 +9,32 @@ import SwiftUI
 import Combine
 
 struct LoginView: View {
-    @State private var login = ""
-    @State private var password = ""
-//    @State private var shouldShowLogo: Bool = true
     
     private let keyboardIsOnPublisher = Publishers.Merge( NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
     .map { _ in true }, NotificationCenter.default.publisher(for:
     UIResponder.keyboardWillHideNotification) .map { _ in false }
     )
     .removeDuplicates()
+    
+    @State private var login = ""
+    @State private var password = ""
+    @State private var showIncorrentCredentialsWarning: Bool = false
+    @Binding var isUserLoggedIn: Bool
+//    @State private var shouldShowLogo: Bool = true
+    
+ 
+    private func verifyLoginData() {
+    if login == "Admin" && password == "123456" {
+        isUserLoggedIn = true
+    } else {
+    showIncorrentCredentialsWarning = true
+    }
+    password = ""
+    }
+    
+//    init(isUserLoggedIn: Binding<Bool>) {
+//        self.isUserLoggedIn = isUserLoggedIn
+//    }
     
     var body: some View {
         ScrollView (showsIndicators: false) {
@@ -39,28 +56,30 @@ struct LoginView: View {
 //                    Spacer()
                     SecureField("Password", text: $password) .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
-                Button(action: { print("Hello") }) {
+                Button(action: verifyLoginData) {
                     Text("Log in") }.frame(width: 124, height: 36).foregroundColor(.white).background(Color(red: 1.00, green: 0.68, blue: 0.36))
                 .disabled(login.isEmpty || password.isEmpty)
                 }.frame(maxWidth: 264).padding(.top, 36)
-            }
-            HStack {
-                Image("001")
-                    .resizable()
-                    .frame(width: 60, height: 60)
-                    .cornerRadius(30)
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text ("Самый темный час – перед рассветом")
-                            .font(.system(size: 16, weight: .bold))
-                        }
-                    Spacer().frame(height:6)
-                    HStack {
-                            Text("Пауло Коэльо").font(.system(size:14))
-                        }
-                    }
-        }.padding(6)
+            }.onTapGesture { UIApplication.shared.endEditing()
+            }.alert(isPresented: $showIncorrentCredentialsWarning, content: { Alert(title: Text("Error"), message: Text("Incorrent Login/Password was entered"))
+            })
+//            HStack {
+//                Image("001")
+//                    .resizable()
+//                    .frame(width: 60, height: 60)
+//                    .cornerRadius(30)
+//                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+//                VStack(alignment: .leading) {
+//                    HStack {
+//                        Text ("Самый темный час – перед рассветом")
+//                            .font(.system(size: 16, weight: .bold))
+//                        }
+//                    Spacer().frame(height:6)
+//                    HStack {
+//                            Text("Пауло Коэльо").font(.system(size:14))
+//                        }
+//                    }
+//        }.padding(6)
         }
         .onReceive(keyboardIsOnPublisher) { isKeyboardOn in withAnimation(Animation.easeInOut(duration: 0.5)) {
 //        self.shouldShowLogo = !isKeyboardOn
@@ -71,10 +90,3 @@ struct LoginView: View {
 extension UIApplication { func endEditing() {
 sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
 } }
-
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
